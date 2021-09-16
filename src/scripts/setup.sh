689d6df030fd01512f9ecf7e6691371d01d17937
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2021-09-16 11:49:17 +0100

    Use correct param names

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,12 +1,11 @@
 Install() {
     PARAM_DD_API_KEY=$(eval echo "\$$PARAM_DD_API_KEY")
-    DD_API_KEY=${PARAM_DD_API_KEY} DD_AGENT_MAJOR_VERSION=${DD_AGENT_MAJOR_VERSION} DD_SITE=${DD_SITE} \
+    DD_API_KEY=${PARAM_DD_API_KEY} DD_AGENT_MAJOR_VERSION=${PARAM_DD_AGENT_MAJOR_VERSION} DD_SITE=${PARAM_DD_SITE} \
         DD_HOSTNAME="none" DD_INSTALL_ONLY="true" DD_APM_ENABLED="true" \
         bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 
     if [ "$UID" = "0" ]; then export SUDO=''; else export SUDO='sudo'; fi
     $SUDO find /etc/datadog-agent/conf.d/ -iname "*.yaml.default" -delete
-    $SUDO echo "apm_config:\n  enabled: true" >> /etc/datadog-agent/datadog.yaml
     $SUDO service datadog-agent start
 }
 
