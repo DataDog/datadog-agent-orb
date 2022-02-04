e287f819492c6221fa0a3ee48706cf52e0e750b1
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-02-04 15:20:28 +0000

    [semver:skip] Wait until agent is ready before continuing (#10)
    * Wait until agent is ready before continuing
    
    * Update sleep time
    
    * Increase wait time
    
    * Cap wait time to 60 seconds
    
    * Use health command exit code
    
    Co-authored-by: Adrián López Calvo <adrianlopezcalvo@gmail.com>
    
    * Streamline start script
    
    * Add other OSs integration tests
    
    * Try to fix comparison
    
    Co-authored-by: Adrián López Calvo <adrianlopezcalvo@gmail.com>

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -12,6 +12,23 @@ Install() {
     if [ "$UID" = "0" ]; then export SUDO=''; else export SUDO='sudo'; fi
     $SUDO find /etc/datadog-agent/conf.d/ -iname "*.yaml.default" -delete
     $SUDO service datadog-agent start
+
+    set +e
+    attempts=0
+
+    until [[ $attempts -eq 10 ]] || $SUDO datadog-agent health; do
+        attempts=$((attempts+1))
+        sleep_time=$(( attempts*5 < 30 ? attempts*5 : 30 ))
+        echo "Waiting for agent to start up sleeping for ${sleep_time} seconds"
+        sleep $sleep_time
+    done
+
+    if [[ $attempts -eq 10 ]]; then
+        echo "Could not start the agent"
+        exit 1
+    else
+        echo "Agent is ready"
+    fi
 }
 
 # Will not run if sourced for bats-core tests.
